82cf9ba2c4d36e50f2501cdaf910a955e76466f8
ytaousi <yassir.taous@gmail.com>
2023-09-22 19:23:26 +0100

    part 2 finished

--- p1/script.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-cp /usr/share/doc/quagga/zebra.conf.sample /usr/share/doc/quagga/ospfd.conf.sample /etc/quagga/
-mv /etc/quagga/zebra.conf.sample /etc/quagga/zebra.conf
-mv /etc/quagga/ospfd.conf.sample /etc/quagga/ospfd.conf
-rc-update add zebra
-rc-update add ospfd
